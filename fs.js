7724557e40aff1f6f8c12a84760f55318f00a726
ajayrai03 <ajayrai1012@gmail.com>
2022-03-06 14:05:11 +0530

    seven

--- fs.js
+++ fs.js
@@ -4,18 +4,16 @@
 //     age     : 50,
 //     eyeColor  : "blue"
 //   }
-  
+
 //   console.log(person);
-function outer()
-{
-  var a=7;
-  function inner()
-  {
+function outer() {
+  var a = 7;
+  function inner() {
     console.log(a);
   }
-  a=10;
+  a = 10;
   return inner;
 }
-var c=outer();
+var c = outer();
 console.log(c);
 c();
